5181b36ca6d92ad4a2f2a8456ee0b731c88666ad
tomoyukiHAYAKAWA <x14091xx@aitech.ac.jp>
2018-11-28 12:42:33 +0900

    first commit

---
+++ gitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTestProject
+//
+//  Created by Tomoyuki Hayakawa on 2018/11/28.
+//  Copyright © 2018 Tomoyuki Hayakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
